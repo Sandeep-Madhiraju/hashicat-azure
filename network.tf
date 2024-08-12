bdd6324fbc3f1252b17f853bc47d9ae94e513ff4
Sandeep-Madhiraju <87055139+Sandeep-Madhiraju@users.noreply.github.com>
2024-08-12 13:36:35 +0530

    Update network.tf

--- network.tf
+++ network.tf
@@ -1,5 +1,5 @@
 module "network" {
   source  = "app.terraform.io/workshop-12aug2024/network/azurerm"
   version = "3.5.0"
-  resource_group_name = "myresourcegroup"
+  resource_group_name = "vnet"
 }
